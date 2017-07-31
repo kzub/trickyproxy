28ff812c276020df48cb7cb7e3aa8041acc2da94
Zubkov Konstantin <konstantin.zubkov@onetwotrip.com>
2017-07-31 11:11:59 +0300

    change version

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 type resultStatus int
 
 const (
-	version  string       = "2.0.0"
+	version  string       = "2.0.1"
 	success  resultStatus = iota
 	fail     resultStatus = iota
 	notFound resultStatus = iota
